07bc113c0504ca37b8a087475b658345ebba32fb
adamstallard <adam@sweedl.com>
2012-07-25 19:54:59 -0700

    change comments

--- test/all.js
+++ test/all.js
@@ -1,8 +1,10 @@
-// all tests
+// options
 
 var spec = require('../node_modules/vows/lib/vows/reporters/spec');
 var opts = {
   reporter : spec
 };
+
+// all tests
 
 require('./ModestCompilerTest').ModestCompiler.run(opts);
