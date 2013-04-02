5a6602dadfab936f456d1a6d5d23be82d4e786f9
adamstallard <adam@goalzen.org>
2013-04-01 21:33:49 -0700

    add gruntfile and jshintrc

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // project configuration
+  grunt.initConfig({
+    jshint : {
+      all : {
+        src : [
+          "*.js",
+          "bin/**/*.js",
+          "lib/**/*.js",
+          "test/**/*.js"
+        ]
+      },
+      options : {
+        jshintrc : ".jshintrc"
+      }
+    },
+    vows : {
+      options : {
+        reporter : "spec"
+      },
+      compiler : {
+        src : ['test/ModestCompilerTests.js'],
+        options : {
+          disabled : false
+        }
+      }
+    }
+  });
+
+  // npm tasks
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-vows-runner');
+
+  // default task
+  grunt.registerTask('default', ['vows']);
+
+};
